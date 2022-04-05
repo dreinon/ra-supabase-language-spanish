7216b56cdab842665fc4c86a4070f38fe35fe3e5
Dani Reinón <dani@dribo.es>
2022-04-05 12:31:37 +0200

    feat: package.json, tsconfig and translations

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export const raSupabaseSpanishMessages = {
+    'ra-supabase': {
+        auth: {
+            email: 'Email',
+            confirm_password: 'Confirmar contraseña',
+        },
+    },
+};
